e8b92bc9e6d1cd8143ca1332d19155a20333885a
Jan Vlcek <jan.vlcek@i.cz>
2012-10-29 16:22:51 +0100

    maximum_pole() sice elisce funguje, ale obsahuje dost chyb, zjevnych, potencialnich

--- vlckova06.c
+++ vlckova06.c
@@ -4,16 +4,18 @@ jo, dojeli jsme az ted, jeli jsme pres hranice a navic oni nemaj dalnicni znamku
 mam jednicku.
 */
 
+//chyba: chybi #include <stdio.h> !!!
 int main (void) {
 
-int i;
+int i; //chyba: tuto promennou definovat ve funkci maximum_pole() !
 int pole[] = {20, -250, -20, 5, 89, 1, 56, 189, -2, 5, 9, 15, 687, 32, 31};
 
-int maximum_pole (int cisla[], int delka_pole) {
+int maximum_pole (int cisla[], int delka_pole) { //chyba: definujes funkci ve funkci! GNU C to sice snese/umi, ale!!!
 
    int max = cisla[0];
-
-   for (i = 0; i < delka_pole; i++)
+//chyba? 1. protoze max jsi inicialozovala na cislo[0], nemusis for () cyklus inicialovat i=0, ale i=1
+//       2. melo by se kontrolovat, je-li delka_pole >=1, delka <=0 je nesmysl
+   for (i = 0; i < delka_pole; i++) //chyba? ja bych dal { ... } i kolem vsech jedno-prikazu
        if (pole[i] > max)
 	  max = pole[i];
    return max;
